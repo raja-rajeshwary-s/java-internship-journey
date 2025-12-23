86ab7e83e273bfa95a74ac799a09e8518d6e462b
Raja Rajeshwary.s <rajarajeshwary.s.2024.aiml@rajalakshmi.edu.in>
2025-12-23 17:56:26 +0530

    Add ArithmeticOperations class for basic calculations

---
+++ ArithmeticOperations.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+
+        int a = 9;
+        int b = 8;
+
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Difference = " + (a - b));
+        System.out.println("Product = " + (a * b));
+        System.out.println("Quotient = " + (a / b));
+    }
+}

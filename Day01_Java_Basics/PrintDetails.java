d640d438532c866ec66f703b107d34a4d9b15583
Raja Rajeshwary.s <rajarajeshwary.s.2024.aiml@rajalakshmi.edu.in>
2025-12-24 09:13:22 +0530

    Add PrintDetails program

---
+++ Day01_Java_Basics/PrintDetails.java
@@ -0,0 +1,10 @@
+public class PrintDetails {
+    public static void main(String[] args) {
+
+        System.out.println("Name: Raji");
+        System.out.println("College: Rajalakshmi Engineering College");
+        System.out.println("Department: AIML");
+        System.out.println("Year: 2");
+
+    }
+}
